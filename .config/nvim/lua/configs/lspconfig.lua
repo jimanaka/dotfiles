25840ac52c949bc85d4d42a3eff51849ddeba019
Jake Imanaka <jake.imanaka@gmail.com>
2024-03-14 01:51:48 -1000

    finalize nvchad v2.5

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = configs.on_init
 local capabilities = configs.capabilities
 
 local lspconfig = require("lspconfig")
--- local util = require("lspconfig/util")
+local util = require("lspconfig/util")
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "html", "cssls", "tsserver", "clangd", "pylsp", "tailwindcss", "ltex" }
